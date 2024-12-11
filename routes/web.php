2b5465603f2fdd2b16c46812e0efe52faf61047d
Owen <osanders@PRE3571-F8ZTSL3.hhf.local>
2024-12-11 20:01:57 +0000

    Move controller into main folder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\AuthenticatedSessionController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserExpensesController;
 use Illuminate\Support\Facades\Route;
