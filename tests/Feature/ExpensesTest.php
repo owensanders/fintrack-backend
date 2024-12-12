e7c4ee3d0ef4f3c24930863d6ed229eda9f146d1
Owen <osanders@PRE3571-F8ZTSL3.hhf.local>
2024-12-12 15:47:51 +0000

    Add back some tests

--- tests/Feature/ExpensesTest.php
+++ tests/Feature/ExpensesTest.php
@@ -33,6 +33,157 @@ class ExpensesTest extends TestCase
         ]);
     }
 
+    public function test_user_cannot_create_expense_if_expense_name_missing(): void
+    {
+        $user = User::factory()->create();
+        $expenseName = null;
+        $expenseAmount = 98732.23;
+
+        $response = $this->actingAs($user)->postJson('user-expenses', [
+            'user_id' => $user->id,
+            'expense_name' => $expenseName,
+            'expense_amount' => $expenseAmount,
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertDatabaseMissing('user_expenses', [
+            'user_id' => $user->id,
+            'expense_name' => $expenseName,
+            'expense_amount' => $expenseAmount,
+        ]);
+    }
+
+    public function test_user_cannot_create_expense_if_expense_amount_missing(): void
+    {
+        $user = User::factory()->create();
+        $expenseName = $this->faker->word;
+        $expenseAmount = null;
+
+        $response = $this->actingAs($user)->postJson('user-expenses', [
+            'user_id' => $user->id,
+            'expense_name' => $expenseName,
+            'expense_amount' => $expenseAmount,
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertDatabaseMissing('user_expenses', [
+            'user_id' => $user->id,
+            'expense_name' => $expenseName,
+            'expense_amount' => $expenseAmount,
+        ]);
+    }
+
+    public function test_user_cannot_create_expense_if_expense_amount_format_is_incorrect(): void
+    {
+        $user = User::factory()->create();
+        $expenseName = $this->faker->word;
+        $expenseAmount = 1622.1;
+
+        $response = $this->actingAs($user)->postJson('user-expenses', [
+            'user_id' => $user->id,
+            'expense_name' => $expenseName,
+            'expense_amount' => $expenseAmount,
+        ]);
+
+        $response->assertStatus(422);
+        $this->assertDatabaseMissing('user_expenses', [
+            'user_id' => $user->id,
+            'expense_name' => $expenseName,
+            'expense_amount' => $expenseAmount,
+        ]);
+    }
+
+    public function test_user_can_delete_an_expense(): void
+    {
+        $user = User::factory()->create();
+        $expenseName = $this->faker->word;
+        $expenseAmount = 766.23;
+
+        $response = $this->actingAs($user)->postJson('user-expenses', [
+            'user_id' => $user->id,
+            'expense_name' => $expenseName,
+            'expense_amount' => $expenseAmount,
+        ]);
+
+        $response->assertStatus(201);
+        $this->assertDatabaseHas('user_expenses', [
+            'user_id' => $user->id,
+            'expense_name' => $expenseName,
+            'expense_amount' => $expenseAmount,
+        ]);
+
+        $expenseId = $user->expenses[0]['id'];
+
+        $response = $this->actingAs($user)->deleteJson('user-expenses/' . $expenseId);
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('user_expenses', [
+            'id' => $expenseId,
+            'user_id' => $user->id,
+            'expense_name' => $expenseName,
+            'expense_amount' => $expenseAmount,
+        ]);
+    }
+
+    public function test_user_can_update_an_expense(): void
+    {
+        $user = User::factory()->create();
+        $originalExpenseName = $this->faker->word;
+        $originalExpenseAmount = 429922.23;
+
+        $originalExpense = UserExpense::factory()->create([
+            'user_id' => $user->id,
+            'expense_name' => $originalExpenseName,
+            'expense_amount' => $originalExpenseAmount,
+        ]);
+
+        $expenseId = $originalExpense->id;
+        $newExpenseName = $this->faker->word;
+        $newExpenseAmount = 600.53;
+
+        $response = $this->actingAs($user)->putJson('user-expenses/' . $expenseId, [
+            'id' => $expenseId,
+            'expense_name' => $newExpenseName,
+            'expense_amount' => $newExpenseAmount,
+        ]);
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('user_expenses', [
+            'id' => $expenseId,
+            'user_id' => $user->id,
+            'expense_name' => $newExpenseName,
+            'expense_amount' => $newExpenseAmount,
+        ]);
+    }
+
+    public function test_user_cannot_update_an_expense_if_expense_name_is_missing(): void
+    {
+        $user = User::factory()->create();
+        $originalExpenseName = $this->faker->word;
+        $originalExpenseAmount = 3456.76;
+
+        $originalExpense = UserExpense::factory()->create([
+            'user_id' => $user->id,
+            'expense_name' => $originalExpenseName,
+            'expense_amount' => $originalExpenseAmount,
+        ]);
+
+        $expenseId = $originalExpense->id;
+        $newExpenseName = null;
+        $newExpenseAmount = 1400.22;
+
+        $response = $this->actingAs($user)->putJson('user-expenses/' . $expenseId, [
+            'id' => $expenseId,
+            'expense_name' => $newExpenseName,
+            'expense_amount' => $newExpenseAmount,
+        ]);
+        $response->assertStatus(422);
+        $this->assertDatabaseMissing('user_expenses', [
+            'id' => $expenseId,
+            'user_id' => $user->id,
+            'expense_name' => $newExpenseName,
+            'expense_amount' => $newExpenseAmount,
+        ]);
+    }
+
     public function test_user_cannot_delete_non_existing_expense(): void
     {
         $user = User::factory()->create();
